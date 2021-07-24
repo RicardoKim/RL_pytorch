4ee438d71a46aaaa7e0aa3a24317ce51ae0a6fab
RicardoKim <may3210@naver.com>
2021-07-24 22:18:45 +0900

    Add Readme in PPO directory

---
+++ PPO/README.md
@@ -0,0 +1,34 @@
+# PPO
+
+현재까지는 state는 1차원 벡터, action은 continuous한 환경에서만 작동합니다.
+
+## 파일 설명 
+
+PPO.py : PPO 알고리즘이 구현된 파일입니다.
+
+Argument.py : PPO 알고리즘에서 사용된 하이퍼 파라미터를 관리하는 파일입니다.
+
+main.py : PPO 알고라즘을 실행하는 파일입니다.
+
+Model/MLPMODEL : actor와 critic network가 정의된 파일입니다.
+
+## 학습 실행 방법
+
+```
+python3 main.py --env 환경 이름
+```
+
+## 결과 보기
+
+```
+python Result_visualize.py --type 원하는 결과
+```
+원하는 결과
+
+1. actor_loss
+2. critic_loss
+3. episode_reward
+
+
+## 실험 결과
+
---
+++ PPO/Result_visualize.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+import argparse
+import matplotlib.pyplot as plt
+
+def visualize(args):
+    log_dir = os.getcwd() + "/Log"
+    file_list = os.listdir(log_dir)
+    result = pd.read_csv(log_dir + "/" + file_list[0])
+    plt.plot(result[args.type])
+    directory = "Result"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    plt.savefig(directory + "/ " + args.type + ".png")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Result visualize.')
+    parser.add_argument('--type', help="What do you want to visualize")
+
+    args = parser.parse_args()
+    visualize(args)
