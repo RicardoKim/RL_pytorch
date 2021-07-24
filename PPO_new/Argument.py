97438e05643843f36a68e4d2f1729ed5da71a901
jinhwan <ricardokim@LAPTOP-A8THLKEC.localdomain>
2021-07-24 20:31:06 +0900

    PPO_refactoring

---
+++ PPO_new/Argument.py
@@ -0,0 +1,8 @@
+class argument():
+    def __init__(self):
+        self.GAMMA = 0.95
+        self.BATCH_SIZE = 64
+        self.EPS_CLIP = 0.2
+        self.ACTOR_LEARNING_RATE = 3e-5
+        self.CRITIC_LEARNING_RATE = 1e-5
+        self.TRAIN_EPOCH = 500
